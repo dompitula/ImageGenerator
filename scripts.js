82fdf5a9f1dd5e67d6529f6181ddbf2034f17c3e
dom <dom@pitula.pl>
2023-05-17 11:53:29 +0200

    image generator by phrase - base +backend complete

---
+++ scripts.js
@@ -0,0 +1,63 @@
+$(function(){
+
+   $(function() {
+      $(":file").change(function() {
+        if (this.files && this.files[0]) {
+          for (var i = 0; i < 3; i++) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[i]);
+          }
+        }
+      });
+    });
+    
+    function imageIsLoaded(e) {
+      $('#myImg').append('<img src=' + e.target.result + '>');
+    };
+
+	var clientID = "vBZPA1MHSw5Hu0IAlHJf1aPY5vevEtQ15CODlC43oII";
+
+	var getImage = function()
+	{
+		var image = $('#term').val();
+		var number = $('#quantity').val();
+
+		if (image == '') 
+		{
+			$('#image0').html("<h2 class='loading'>Proszę wprowadzić poprawne słowo wyszukania.</h2>");
+		} 
+		else 
+		{
+
+			$('#image0').html("<h2 class='loading'>Trwa ładowanie obrazów...</h2>");
+
+			$.getJSON(`https://api.unsplash.com/search/photos?lang=pl&page=1&query=${image}&client_id=${clientID}`,
+				function(jsonData) {
+				// console.log(jsonData);
+				// console.log(jsonData.results[0].urls.regular);
+            	// console.log(jsonData.results[1].urls.regular);
+
+					if (jsonData.total > 0) 
+					{
+						console.log(number);
+						for (let i = 0; i <= number; i++)
+						{
+							$(`#image${i}`).html('<img id="theImage" src=' + jsonData.results[i].urls.regular + ' />');
+						}
+					} 
+					else if (jsonData.total == 0)
+					{
+						$.getJSON(`https://api.unsplash.com/search/photos?lang=pl&page=1&query=cat&client_id=${clientID}`, 
+							function(jsonData){
+								$('#image0').html('<h2 class="loading">Nie udało się odnaleźć pasujących obrazów, a może chodziło ci o zdjęcia kota?</h2><img id="theImage" src=' + jsonData.results[0].urls.regular + '/>');
+	                    });                 
+	                }
+			});
+		}
+
+		return false;	
+	}
+
+	$('#fetch form').on('submit', getImage);
+});
